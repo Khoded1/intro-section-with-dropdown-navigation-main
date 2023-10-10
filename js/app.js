846dd92f7d536221a8cbdc637b0e97f10e674539
khodedcodeit <126296057+Khoded1@users.noreply.github.com>
2023-10-10 21:00:57 +0100

    Add files via upload
    intro-section-with-dropdown-navigation-main 
    
    still working on it

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const headerEl = document.querySelector('.header');
+const upArrowEl = document.querySelector('.up-arrow');
+const downArrowEl = document.querySelector('.down-arrow');
+const featureSubEl = document.querySelector('.features_sub_nav_container');
+const companySubEl = document.querySelector('.company_sub_nav_container');
+const featureEL = document.getElementById('feature_link');
---
+++ txt.md
@@ -0,0 +1,3 @@
+Features Company Careers About Login Register Make remote work Get your
+team in sync, no matter your location. Streamline processes, create team
+rituals, and watch productivity soar. Learn more
